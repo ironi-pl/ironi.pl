5a3700dedc5766ea1c9890a9998041851f90c6cf
ironi <ironi@ironi.pl>
2026-06-23 13:55:44 +0200

    Add Cloudflare adapter to Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: cloudflare(),
   vite: {
     server: {
       host: "0.0.0.0",
